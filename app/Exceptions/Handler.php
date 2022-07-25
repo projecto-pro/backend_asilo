35f44823a9eba75d8d433926f2d78432297c2009
RenHec <emisor.tarea@gmail.com>
2022-07-25 16:40:46 -0600

    Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -86,67 +86,6 @@ class Handler extends ExceptionHandler
         }
 
         if ($exception instanceof QueryException) {
-            if (Config::get('database.default') == 'pgsql')
-                $codigo = $exception->errorInfo[0];
-            else
-                $codigo = $exception->errorInfo[1];
-
-            $message = "";
-
-            switch ($codigo) {
-                case 4060:
-                    $message = "No se puede abrir la base de datos solicitada por el inicio de sesión";
-                    break;
-                case 40197:
-                    $message = "Error en el servicio al procesar la solicitud. Vuelva a intentarlo";
-                    break;
-                case 40501:
-                    $message = "El servicio está ocupado actualmente. Vuelva a intentar la solicitud después de 10 segundos";
-                    break;
-                case 40613:
-                    $message = "Vuelva a intentar la conexión más tarde";
-                    break;
-                case 49918:
-                    $message = "No se puede procesar la solicitud. No hay suficientes recursos para procesar la solicitud";
-                    break;
-                case 49919:
-                    $message = "No se procesar, crear ni actualizar la solicitud. Hay demasiadas operaciones de creación o actualización en curso para la suscripción";
-                    break;
-                case 1451:
-                    $message = "El registro se encuentra asociado, no podra realizar la eliminación";
-                    break;
-                case 104:
-                    $message = "Debe haber elementos ORDER BY en la lista de selección si la instrucción contiene el operador UNION, INTERSECT o EXCEPT";
-                    break;
-                case 107:
-                    $message = "El prefijo de columna no coincide con un nombre de tabla o un nombre de alias utilizado en la consulta";
-                    break;
-                case 109:
-                    $message = "Hay más columnas en la instrucción INSERT que valores en la cláusula VALUES. El número de valores de VALUES debe coincidir con el de columnas de INSERT";
-                    break;
-                case 109:
-                    $message = "Hay más columnas en la instrucción INSERT que valores en la cláusula VALUES. El número de valores de VALUES debe coincidir con el de columnas de INSERT";
-                    break;
-                case 1048:
-                    $message = "Los campos no pueden ser nulos, debe de especificar un valor";
-                    break;
-                case 42703:
-                    $message = "No existe una o varias columnas en la BD y que están referenciadas en el modelo";
-                    break;
-                case 1054:
-                    $message = "No se encuentra la columna en la tabla";
-                    break;
-                case 1241:
-                    $message = "El operando debe contener 1 columna (s)";
-                    break;
-                case 1604:
-                    $message = "Error en el query";
-                    break;
-                default:
-                    $message = "BD";
-                    break;
-            }
-
             return $this->errorResponse($exception->errorInfo, 422);
         }
 
