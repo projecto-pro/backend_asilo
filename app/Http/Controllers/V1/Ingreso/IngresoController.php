5cceeee572ceb66723d8f669471518fbbf0e754f
RenHec <emisor.tarea@gmail.com>
2022-07-25 20:52:31 -0600

    AuthController

--- app/Http/Controllers/V1/Ingreso/IngresoController.php
+++ app/Http/Controllers/V1/Ingreso/IngresoController.php
@@ -41,7 +41,6 @@ class IngresoController extends ApiController
     {
         try {
             DB::beginTransaction();
-            return $this->successResponse($request);
 
             $protegido = $this->protegido($request->paciente);
             $responsable = $this->persona($request->responsable);
